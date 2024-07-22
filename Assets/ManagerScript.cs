dc6ef7f642a66f702c63be487281bbe851a2e198
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-22 18:40:24 -0400

    Commit #11

--- Assets/ManagerScript.cs
+++ Assets/ManagerScript.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class ManagerScript : MonoBehaviour
 {
     public GameObject selected;
+    public float gridSize=1f;
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            place(selected);
+        }
     }
+    void selectBuilding(GameObject sel)
+    {
+        selected = sel;
+    }
+    void place(GameObject s)
+    {
+        Instantiate(s);
+    }
+
 }
