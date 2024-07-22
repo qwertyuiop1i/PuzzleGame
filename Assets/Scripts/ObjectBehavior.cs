d564c03db8796dfb3dbc2bb6f81ef8aa59c46b46
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-22 15:14:09 -0400

    Commit #8

---
+++ Assets/Scripts/ObjectBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehavior : MonoBehaviour
+{
+    public float mass;
+    public Rigidbody2D rb;
+    public float gravity;
+      
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
