5fc0906e6e8c36403d8ee227d5146ac15b31242a
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-23 08:01:41 -0400

    Commit #16

---
+++ Assets/wireCode.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wireCode : MonoBehaviour
+{
+    public GameObject nextTo;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //detect if adjacent to wires, and if one wire in a chain is tocuhing the light then light needs to light up
+    }
+}
