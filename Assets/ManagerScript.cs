6495a2790cab7d673e50fa4eaf38014382621b4d
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-22 21:35:17 -0400

    Commit #13

--- Assets/ManagerScript.cs
+++ Assets/ManagerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ManagerScript : MonoBehaviour
 {
     public GameObject selected;
+    public GameObject grid;
     public float gridSize = 1f;
 
     void Update()
@@ -25,14 +26,21 @@ public class ManagerScript : MonoBehaviour
         if (s == null)
             return;
 
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // Snap to grid
         float gridX = Mathf.RoundToInt(mousePos.x / gridSize) * gridSize;
         float gridY = Mathf.RoundToInt(mousePos.y / gridSize) * gridSize;
         Vector2 gridPosition = new Vector2(gridX, gridY);
 
- 
-        Instantiate(s, gridPosition, Quaternion.identity);
+        Collider2D overlap = Physics2D.OverlapBox(gridPosition, s.GetComponent<Collider2D>().bounds.size, 0f);
+        if (overlap != null)
+        {
+           
+            Debug.Log("Collision detected!");
+            return;
+        }
+
+        GameObject temp = Instantiate(s, gridPosition, Quaternion.identity);
+        temp.transform.parent = grid.transform;
     }
 }
